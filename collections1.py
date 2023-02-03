2e3ec1b4c7a74a087d8ed5990ebe88cb6c755af4
Bruno <brox.fs@gmail.com>
2023-02-02 21:59:19 -0300

    Initial commit.

---
+++ collections1.py
@@ -0,0 +1,34 @@
+# idades = [39, 30, 27, 18]
+#
+#
+# print(len(idades))
+#
+# idades.remove(30)
+#
+# idades.clear()
+#
+# print(idades)
+
+class ContaCorrente:
+
+    def __init__(self, codigo):
+        self.codigo = codigo
+        self.saldo = 0
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def __str__(self):
+        return f'Código: {self.codigo} \n Saldo: {self.saldo}'
+
+
+conta1 = ContaCorrente(15)
+conta2 = ContaCorrente(23)
+
+conta1.deposita(500)
+conta2.deposita(1200)
+
+contas = [conta1, conta2]
+
+for conta in contas:
+    print(conta)
